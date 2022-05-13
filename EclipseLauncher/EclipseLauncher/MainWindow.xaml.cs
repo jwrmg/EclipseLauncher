9509eab2f478a2d8f312b4198a4b9763bd775a01
jwrmg <69098500+jwrmg@users.noreply.github.com>
2022-05-13 12:29:29 +1000

    Begun basic launcher design

--- EclipseLauncher/EclipseLauncher/MainWindow.xaml.cs
+++ EclipseLauncher/EclipseLauncher/MainWindow.xaml.cs
@@ -1,17 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections.ObjectModel;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace EclipseLauncher
 {
@@ -20,14 +11,57 @@ namespace EclipseLauncher
     /// </summary>
     public partial class MainWindow : Window
     {
+        public SolidColorBrush GoldColor { get; set; } = new(Color.FromArgb(255, 255, 133, 0));
+        public SolidColorBrush PurpleColor { get; set; } = new(Color.FromArgb(255, 14, 0, 17));
+        public SolidColorBrush LightPurpleColor { get; set; } = new(Color.FromArgb(255, 62, 51, 64));
+        public SolidColorBrush TextColor { get; set; } = new(Color.FromArgb(255, 255, 255, 255));
+
+        public int HeaderFontSize { get; set; } = 32;
+        public int StandardFontSize { get; set; } = 16;
+
+        private ObservableCollection<Project> m_Projects;
+
+        public ObservableCollection<Project> Projects
+        {
+            get { return m_Projects ??= new(); }
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+            this.DataContext = this;
+
+            for (int i = 0; i < 100; i++)
+            {
+                Projects.Add(new Project());
+            }
         }
 
         private void BtnAdd_OnClick(object sender, RoutedEventArgs e)
         {
-          
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void MaximizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void MinimizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void TitleBar_OnMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                if (Application.Current.MainWindow != null) Application.Current.MainWindow.DragMove();
+            }
         }
     }
 }
---
+++ EclipseLauncher/EclipseLauncher/Project.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EclipseLauncher
+{
+    public class Project
+    {
+        public string name { get; set; } = "TEST_PROJECT";
+    }
+}
